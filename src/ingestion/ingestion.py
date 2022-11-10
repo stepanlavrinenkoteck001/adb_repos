4300ba47d92bfaa3b515e8bdc1224c6dd70ba294
stepanlavrinenkoteck001 <85255366+stepanlavrinenkoteck001@users.noreply.github.com>
2022-11-10 16:00:21 -0700

    Update ingestion.py

--- src/ingestion/ingestion.py
+++ src/ingestion/ingestion.py
@@ -1,6 +1,6 @@
 from pyspark.context import SparkContext
 from pyspark.sql.session import SparkSession
-sc = SparkContext('local')
+sc = SparkContext.getOrCreate()
 spark = SparkSession(sc)
 
 class blastIqIngest:
