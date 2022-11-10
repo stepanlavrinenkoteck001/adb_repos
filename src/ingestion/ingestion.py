6fbde648514634b060178835051afbbeee1f716d
stepanlavrinenkoteck001 <85255366+stepanlavrinenkoteck001@users.noreply.github.com>
2022-11-10 15:56:02 -0700

    Update ingestion.py

--- src/ingestion/ingestion.py
+++ src/ingestion/ingestion.py
@@ -3,14 +3,13 @@ class blastIqIngest:
         self.path = path
         
     def ingest(self):
-        try:
-            # read in deafault databricks dataset
-            df = (spark.read.format('delta')
-                .options(header='true', inferSchema='true')
-                .load(self.path))
-            return df
-        except:
-            print('blast Iq data not loaded properly')
+        # read in deafault databricks dataset
+        df = (spark.read.format('delta')
+            .options(header='true', inferSchema='true')
+            .load(self.path))
+        return df
+
+            
         
 
     
