d9fd1d4a02abab8230491fed8b9ed8a9346a20f9
stepanlavrinenkoteck001 <85255366+stepanlavrinenkoteck001@users.noreply.github.com>
2022-11-10 16:19:58 -0700

    Update geom_utils.py

--- src/processing/geom_utils.py
+++ src/processing/geom_utils.py
@@ -15,7 +15,7 @@ class holeGeometry:
     def calculate_hole_volume(self):
         if len(self.geom_columns) == 2:
             i_diameter = [i for i in [0,1] if self.geom_columns[i].find('diameter')>0][0]
-            df_volume = self.df.select(volume_formula(self.df[self.geom_columns[i_diameter]], self.df[self.geom_columns[~i_diameter]]))
+            df_volume = self.df.select(self.volume_formula(self.df[self.geom_columns[i_diameter]], self.df[self.geom_columns[~i_diameter]]))
             df_volume = df_volume.withColumnRenamed(df_volume.schema.names[0], 'volume')
             return df_volume
         else:
